39e638e63dde2fb16c7a8f1a197f0fc76e347d9a
naresh-kumar <nareshnitrkl@gmail.com>
2024-03-12 21:47:58 +0530

    two sum

--- src/main/java/com/leetcode/TwoSum.java
+++ src/main/java/com/leetcode/TwoSum.java
@@ -1,21 +1,26 @@
 package com.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int target = 9;
+        int[] nums = {3,2,4};
+        int target = 6;
         System.out.println("indexes with sum equal to target"+twoSum(nums,target));
+        Map<Integer,Integer> map = new HashMap<>();
     }
 
     public static int[] twoSum(int[] nums, int target) {
         int[] indexes = new int[2];
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    indexes[0] = i;
-                    indexes[1] = j;
-                }
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                indexes[0] = i;
+                indexes[1] = map.get(target-nums[i]).intValue();
+                break;
             }
+            map.put(nums[i],i);
         }
         return indexes;
     }
